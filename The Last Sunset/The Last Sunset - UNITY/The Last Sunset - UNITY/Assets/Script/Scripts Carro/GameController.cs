ab14b7671488acaafd0e2f363bed1c40f7abe647
matsalen <k.kurosaki@hotmail.com>
2019-11-25 17:30:33 -0300

    Barra da policia piscando no HUD

--- The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/Scripts Carro/GameController.cs
+++ The Last Sunset/The Last Sunset - UNITY/The Last Sunset - UNITY/Assets/Script/Scripts Carro/GameController.cs
@@ -14,10 +14,12 @@ public class GameController : MonoBehaviour
     [SerializeField] float aumentoDistancia, velocidadeTexto, escalaInicial, escalaFinal;
     [SerializeField] float tempo = 3;
     [SerializeField] AudioSource aud;
-    [SerializeField] Image policiaBarra;
+    [SerializeField] Image[] policiaBarra;
     [SerializeField] UnityEngine.Rendering.PostProcessing.PostProcessLayer post;
+    [SerializeField] Image piscaAHUD, piscaVHUD;
     LensAberrations lensAberrations;
     Bloom bloom;
+    bool sireneHUD;
     Camera cam;
    
     public float distancia;
@@ -31,8 +33,6 @@ public class GameController : MonoBehaviour
     bool isZoomed = false;
     bool paused = false;
 
-
-
     void Awake()
     {
         if (instancia == null) instancia = this;
@@ -41,8 +41,8 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        
-
+        //StartCoroutine(Pisca());
+        sireneHUD = true;
         cam = Camera.main;
         bloom = cam.GetComponent<Bloom>();
         textoMusica.text = "Colete fitas para ouvir alguma coisa!";
@@ -68,22 +68,39 @@ public class GameController : MonoBehaviour
             textoMusica.transform.localPosition = textoPosInicial;
         }
 
-        policiaBarra.fillAmount = 1 - ((Mathf.Abs(player.position.z) - Mathf.Abs(vanPolicia.position.z) - 10) / 1000);
-        
-        /*
-         * if(aud.clip.name == "-- Nada Tocando --")
+        for (int i = 0; i < 2; i++)
         {
-
+            policiaBarra[i]. fillAmount = 1 - ((Mathf.Abs(player.position.z) - Mathf.Abs(vanPolicia.position.z) - 10) / 500);
+            if (i >= 2)
+                i = 0;
         }
-     
-        */
-        Pausar();
+
+        if (piscaVHUD.enabled)
+        {
+            piscaAHUD.enabled = false;
+        }
+        else piscaAHUD.enabled = true;
+
+        
+        if (policiaBarra[0].fillAmount >= 0.5 && sireneHUD)
+        {
+            StartCoroutine(Pisca());
+            sireneHUD = false;
+        }
+        
+        
+
+
+        
+        
+        //Pausar();
         
     }
 
     public void SaveRecord()
     {
         PlayerPrefs.SetFloat("newrecord", distancia);
+        
     }
 
     public void nitro(float pot)
@@ -139,7 +156,18 @@ public class GameController : MonoBehaviour
         }
     }
 
-   
+    IEnumerator Pisca()
+    {
+        Debug.Log("vsf;");
+        yield return new WaitForSeconds(0.3f);
+        piscaVHUD.enabled = !piscaVHUD.enabled;
+        if (policiaBarra[0].fillAmount <= 0.49)
+        {
+            sireneHUD = true;
+        }
+        else
+            StartCoroutine(Pisca());
+    }
 }
 
 
